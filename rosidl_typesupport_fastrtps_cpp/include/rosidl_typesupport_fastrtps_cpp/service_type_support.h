6c41b71acf1c858618976606eb2b96ef94c475e2
Stephen Brawner <brawner@gmail.com>
2020-05-12 16:42:47 -0700

    Updating public API documentation
    Signed-off-by: Stephen Brawner <brawner@gmail.com>

--- rosidl_typesupport_fastrtps_cpp/include/rosidl_typesupport_fastrtps_cpp/service_type_support.h
+++ rosidl_typesupport_fastrtps_cpp/include/rosidl_typesupport_fastrtps_cpp/service_type_support.h
@@ -21,12 +21,17 @@
 
 #include "rosidl_typesupport_fastrtps_cpp/message_type_support.h"
 
+/// Encapsulates the callbacks for getting properties of this rosidl type.
 typedef struct service_type_support_callbacks_t
 {
+  /// The C++ namespace of this service.
   const char * service_namespace_;
+  /// The typename of this service.
   const char * service_name_;
 
+  /// Pointer to the request message typesupport members.
   const rosidl_message_type_support_t * request_members_;
+  /// Pointer to the response message typesupport members.
   const rosidl_message_type_support_t * response_members_;
 } service_type_support_callbacks_t;
 
